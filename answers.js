19ea901c68f6e50cb4ad91820d379539eaf2392f
Coco Moloko <courtneyb.brecht@gmail.com>
2017-01-10 19:27:39 +0000

    Added firstCharacter function

--- answers.js
+++ answers.js
@@ -1,1 +1,10 @@
 //Hello I am a comment!
+
+//Function will return the first character of the string
+function firstCharacter(letter) {
+    return letter[0]
+}
+
+console.log(firstCharacter("Hello"));
+console.log(firstCharacter("Whattup Y'all"));
+console.log(firstCharacter(" "));

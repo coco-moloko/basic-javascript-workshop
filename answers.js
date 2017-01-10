828f4fd56c65bc864aa060cf27aaa64cf4711ad7
Coco Moloko <courtneyb.brecht@gmail.com>
2017-01-10 19:39:02 +0000

    Added lastCharacter function

--- answers.js
+++ answers.js
@@ -8,3 +8,11 @@ function firstCharacter(letter) {
 console.log(firstCharacter("Hello"));
 console.log(firstCharacter("Whattup Y'all"));
 console.log(firstCharacter(" "));
+
+//Function will return the last character of the string
+function lastCharacter(word) {
+    return word[word.length - 1];
+}
+console.log(lastCharacter("Table"));
+console.log(lastCharacter("Chair Legs"));
+console.log(lastCharacter("Pizza "));
